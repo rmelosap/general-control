c56725efb44f1123862b8f8d226a84be2491a069
rmelosap <rmelosap12gt@gmail.com>
2025-11-17 14:11:13 -0300

    Create migration.sql

---
+++ prisma/migrations/0001_init/migration.sql
@@ -0,0 +1,35 @@
+-- Ativar extensão para UUID moderno
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE "User" (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+  pst_grd           VARCHAR(2)   NOT NULL,
+  pst_grd_completo  VARCHAR(5)   NOT NULL,
+
+  quadro            TEXT          NOT NULL,
+  especialidade     TEXT          NOT NULL,
+  funcao            TEXT          NOT NULL,
+  operacionalidade  TEXT          NOT NULL,
+  dependente        TEXT          NOT NULL,
+
+  genero            TEXT          NOT NULL,
+
+  email_funcional   TEXT          NOT NULL,
+  email_particular  TEXT          NOT NULL,
+
+  trigrama          VARCHAR(3)    NOT NULL,
+
+  nome_de_guerra    TEXT          NOT NULL,
+  nome_completo     TEXT          NOT NULL,
+
+  saram             VARCHAR(7)    NOT NULL,
+  cpf               VARCHAR(14)   NOT NULL,
+
+  banco             VARCHAR(3)    NOT NULL,
+  agencia           VARCHAR(10)   NOT NULL,
+  conta_corrente    TEXT          NOT NULL,
+
+  "createdAt" TIMESTAMPTZ NOT NULL DEFAULT now(),
+  "updatedAt" TIMESTAMPTZ NOT NULL DEFAULT now()
+);
